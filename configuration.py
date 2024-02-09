64ba4ac9c057218178bbdfb7c8884d411a488989
OstrogovKirill <156197951+OstrogovKirill@users.noreply.github.com>
2024-02-09 13:37:50 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://6ec52970-faf0-4e04-85dd-fedfef716250.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders"
+GET_ORDER_NUMBER = "/api/v1/orders/track?t="
---
+++ new_order_test.py
@@ -0,0 +1,24 @@
+import configuration
+import data
+import requests
+
+
+def post_create_new_order(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,  # подставляем полный url
+                         json=body)
+
+track = post_create_new_order(data.make_order_body).json()["track"]
+print(track)
+
+def get_order_number(number):
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_NUMBER + str(number))
+response = get_order_number(track)
+print(response.json())
+
+# Кирилл Острогов, 13-я когорта — Финальный проект. Инженер по тестированию плюс
+
+def test_get_order_get_success_response():
+    response = post_create_new_order(data.make_order_body)
+    track = response.json()["track"]
+    get_order = get_order_number(track)
+    assert get_order.status_code == 200
